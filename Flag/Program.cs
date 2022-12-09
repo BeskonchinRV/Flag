3ef9bfd58d3779a42f10e5c3c2daaa97c4dff343
BeskonchinRV <112742675+BeskonchinRV@users.noreply.github.com>
2022-12-09 21:41:13 +0700

    Update Program.cs

--- Flag/Program.cs
+++ Flag/Program.cs
@@ -1,15 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Flag
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+while (true)
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+    Console.WriteLine("Введите месяц: ");
+
+    int month = int.Parse(Console.ReadLine());
+
+    Console.WriteLine((season)month + ": " + whatSeason((season)month));
+}
+string whatSeason(season month)
+{
+    if (month <= (season)2 || month == (season)12) return "Зима" ;
+    if (month <= (season)5) return "Весна";
+    if (month <= (season)8) return "Лето";
+    if (month <= (season)11) return "Осень";
+    else return "Не подходит";
+} 
+
+enum season
+{
+    Январь = 1,
+    Февраль,
+    Март,
+    Апрель,
+    Май,
+    Июнь,
+    Июль,
+    Август,
+    Сентябрь,
+    Октябрь,
+    Ноябрь,
+    Декабрь
 }
